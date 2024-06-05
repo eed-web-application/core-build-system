cc7f560d65a8b7c8b4f532830e09f1f2bc25b224
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-06-05 12:03:10 -0700

    fix testes

--- src/main/java/edu/stanford/slac/core_build_system/repository/KubernetesRepository.java
+++ src/main/java/edu/stanford/slac/core_build_system/repository/KubernetesRepository.java
@@ -1,12 +1,19 @@
 package edu.stanford.slac.core_build_system.repository;
 
-import io.fabric8.kubernetes.api.model.NamespaceBuilder;
-import io.fabric8.kubernetes.api.model.Pod;
-import io.fabric8.kubernetes.api.model.PodBuilder;
+import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.kubernetes.client.dsl.PodResource;
+import io.fabric8.kubernetes.client.utils.Serialization;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @Log4j2
 @Repository
@@ -43,12 +50,36 @@ public class KubernetesRepository {
         return namespaceRes != null;
     }
 
-    public void spinUpPod(String namespace, String podName) {
-        var result = client.pods().inNamespace("ns1").resource(new PodBuilder().withNewMetadata().withName(podName).endMetadata().build()).create();
-        log.info("Pod created: {}", result.getMetadata().getName());
+    /**
+     * Create a new builder pod
+     * @param namespace namespace name
+     * @param builderName component name
+     * @return the newly created podName
+     */
+    public String spinUpBuildPod(String namespace, String builderName) {
+        Pod result = null;
+        ClassPathResource cpResourcePV = new ClassPathResource("pod-builder-template.yaml");
+        try (InputStream inputStream = new FileInputStream(cpResourcePV.getFile())) {
+            Pod pv = Serialization.unmarshal(inputStream , Pod.class);
+            pv.getMetadata().setName("builder-%s".formatted(builderName));
+            pv.getMetadata().setNamespace(namespace);
+            result = client.resource(pv).create();
+            log.info("Pod created: {}", result.getMetadata().getName());
+            return result.getMetadata().getName();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public Pod getPodDescription() {
-        return client.pods().inNamespace("ns1").withName("pod1").get();
+    /**
+     * Get the pod
+     * @return the pod
+     */
+    public PodResource getPod(String namespace, String podName) {
+        return client.pods().inNamespace(namespace).withName(podName);
+    }
+
+    public List<StatusDetails> deletePod(String namespace, String podName){
+        return client.pods().inNamespace(namespace).withName(podName).delete();
     }
 }
--- src/test/java/edu/stanford/slac/core_build_system/repository/KubernetesRepositoryTest.java
+++ src/test/java/edu/stanford/slac/core_build_system/repository/KubernetesRepositoryTest.java
@@ -1,6 +1,11 @@
 package edu.stanford.slac.core_build_system.repository;
 
+import io.fabric8.kubernetes.api.model.PersistentVolume;
+import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.kubernetes.client.utils.Serialization;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,10 +13,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @AutoConfigureMockMvc
@@ -25,11 +37,60 @@ public class KubernetesRepositoryTest {
     KubernetesClient client;
     @Autowired
     KubernetesRepository repository;
+    private final String buildNamespace = "build-ns";
+    @BeforeAll
+    public void initResources() {
+        ClassPathResource cpResourcePV = new ClassPathResource("persistent-volume.yaml");
+        ClassPathResource cpResourcePVC = new ClassPathResource("persistent-volume-claim.yaml");
+        try (InputStream inputStream = new FileInputStream(cpResourcePV.getFile())) {
+            PersistentVolume pv = Serialization.unmarshal(inputStream , PersistentVolume.class);
+            var result = client.resource(pv).create();
+        } catch (KubernetesClientException e) {
+            if(e.getStatus().getReason().compareToIgnoreCase("AlreadyExists") != 0){
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (InputStream inputStream = new FileInputStream(cpResourcePVC.getFile())) {
+            PersistentVolumeClaim pvc = Serialization.unmarshal(inputStream , PersistentVolumeClaim.class);
+            pvc.getMetadata().setNamespace(buildNamespace);
+            var result = client.resource(pvc).create();
+            System.out.println("PV created: " + result.getMetadata().getName());
+        } catch (KubernetesClientException e) {
+            if(e.getStatus().getReason().compareToIgnoreCase("AlreadyExists") != 0){
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // ensure namespace
+        assertDoesNotThrow(()->repository.ensureNamespace(buildNamespace));
+        var result = assertDoesNotThrow(()->repository.existsNamespace(buildNamespace));
+        assertThat(result).isTrue();
+    }
+
 
     @Test
-    public void testK8sPresence() {
-        assertDoesNotThrow(()->repository.ensureNamespace("ns1"));
-        var result = assertDoesNotThrow(()->repository.existsNamespace("ns1"));
-        assertThat(result).isTrue();
+    public void createPodAndWaitForTermination() {
+        String newPodName = assertDoesNotThrow(()->repository.spinUpBuildPod(buildNamespace, "build-1"));
+        await().atMost(3000, SECONDS).pollDelay(2, SECONDS).until(
+                () -> {
+                    var pod = assertDoesNotThrow(()->repository.getPod(buildNamespace, newPodName));
+                    if(pod==null)
+                        return false;
+                    var podSpecific = pod.get();
+                    return podSpecific.getStatus().getContainerStatuses().size()==1 &&
+                    podSpecific.getStatus().getContainerStatuses().getFirst().getState().getTerminated() !=null &&
+                    podSpecific.getStatus().getContainerStatuses().getFirst().getState().getTerminated().getReason().compareToIgnoreCase("Completed") == 0;
+                }
+        );
+        var pod = assertDoesNotThrow(()->repository.getPod(buildNamespace, newPodName));
+        String log = pod.getLog();
+        assertThat(log).isNotEmpty();
+
+        var deletePodResult = assertDoesNotThrow(()->repository.deletePod(buildNamespace, newPodName));
+        assertThat(deletePodResult).isNotEmpty();
     }
 }
